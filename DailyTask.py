c475d9704b4b55be94b92323c15becf8c86f5f20
Tyrique <tvanwyk114@gmail.com>
2020-11-26 19:40:21 +0200

    First commit

---
+++ DailyTask.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+webbrowser.open_new('https://outlook.office.com/mail/inbox')
+webbrowser.open_new_tab('https://www.notion.so/Getting-Started-fcb0e4b7b82a4909b20d270a4e8bc81f')
+webbrowser.open_new_tab('https://mail.google.com/mail/u/0/#inbox')
+webbrowser.open_new_tab('https://github.com/TyriqueVanwyk/')
+
