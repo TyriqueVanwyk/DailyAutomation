bbbab23eab144879bb8df8f5f3fcfb45b4d841be
TyriqueVanwyk <69138103+TyriqueVanwyk@users.noreply.github.com>
2020-11-29 19:53:50 +0200

    Update DailyTask.py

--- DailyTask.py
+++ DailyTask.py
@@ -1,7 +1,7 @@
 import webbrowser
-
-webbrowser.open_new('https://outlook.office.com/mail/inbox')
-webbrowser.open_new_tab('https://www.notion.so/Getting-Started-fcb0e4b7b82a4909b20d270a4e8bc81f')
-webbrowser.open_new_tab('https://mail.google.com/mail/u/0/#inbox')
-webbrowser.open_new_tab('https://github.com/TyriqueVanwyk/')
-
+try:
+  ##You can open any application using the imported module
+  webbrowser.open_new('https://outlook.office.com/mail/inbox')
+  webbrowser.open_new_tab('https://mail.google.com/mail/u/0/#inbox')
+except:
+    pring("An error occured!")
